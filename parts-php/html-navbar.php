f12f07f361b7518a40eefcf73e3368e9e61797ce
furxx2000 <furxx2000@gmail.com>
2021-04-22 13:38:11 +0800

    nomad

---
+++ parts-php/html-navbar.php
@@ -0,0 +1,209 @@
+<div class="side-navbar none">
+  <div class="icon-arrow-up hide">
+    <img src="/icomoon/svg/keyboard_arrow_up.svg" alt="" />
+  </div>
+  <div class="side-navbar-flex flex">
+    <div class="container">
+      <div class="side-navbar-item">
+        <a href="">
+          <div class="navbar-item">
+            <img src="/images/navbar-icon/level.svg" alt="" />
+            <p class="text ff-noto">難度分級</p>
+          </div>
+        </a>
+        <a href="">
+          <div class="navbar-item">
+            <img src="/images/navbar-icon/blog.svg" alt="" />
+            <p class="text ff-noto">登山記事</p>
+          </div>
+        </a>
+        <a href="">
+          <div class="navbar-item">
+            <img src="/images/navbar-icon/notice.svg" alt="" />
+            <p class="text ff-noto">登山須知</p>
+          </div>
+        </a>
+        <a href="">
+          <div class="navbar-item">
+            <img src="/images/navbar-icon/weather.svg" alt="" />
+            <p class="text ff-noto">天氣</p>
+          </div>
+        </a>
+        <a href="">
+          <div class="navbar-item">
+            <img src="/images/navbar-icon/product.svg" alt="" />
+            <p class="text ff-noto">商品</p>
+          </div>
+        </a>
+
+        <a href="">
+          <div class="navbar-item">
+            <img src="/images/navbar-icon/achivement.svg" alt="" />
+            <p class="text ff-noto">成就紀錄</p>
+          </div>
+        </a>
+        <a href="">
+          <div class="navbar-item">
+            <img src="/images/navbar-icon/schedule.svg" alt="" />
+            <p class="text ff-noto">行程</p>
+          </div>
+        </a>
+        <a href="">
+          <div class="navbar-item">
+            <img src="/images/navbar-icon/about_us.svg" alt="" />
+            <p class="text ff-noto">官方資訊</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="icon-arrow-down">
+    <img src="/icomoon/svg/keyboard_arrow_down.svg" alt="" />
+  </div>
+</div>
+
+<div class="navbar">
+  <div class="container flex">
+    <div class="navbar-left">
+      <a href="#">
+        <svg class="icon-nomad-logo-dark navbar-logo-svg">
+          <use
+            xlink:href="./icomoon/symbol-defs.svg#icon-nomad-logo-dark"
+          ></use>
+        </svg>
+      </a>
+    </div>
+    <div class="navbar-right flex">
+      <div class="cart mr-1 none">
+        <svg class="icon-cart svg">
+          <use xlink:href="./icomoon/symbol-defs.svg#icon-cart"></use>
+        </svg>
+      </div>
+
+      <div class="user-icon flex none">
+        <div class="left-box">
+          <img src="/images/User-icon/Ellipse 17.png" alt="" />
+        </div>
+      </div>
+      <div class="hamburger" id="mobile-cta">
+        <input type="checkbox" name="" id="check" />
+        <div class="hamburger-menu-container">
+          <div class="hamburger-menu">
+            <div></div>
+          </div>
+        </div>
+      </div>
+      <nav>
+        <ul>
+          <!-- 可能使用javascript append生成 -->
+          <a href="">
+            <li class="nav-item flex">
+              <p class="ff-noto">官方資訊</p>
+              <svg class="icon-arrow-forward svg">
+                <use
+                  xlink:href="./icomoon/symbol-defs.svg#icon-arrow_forward_ios"
+                ></use>
+              </svg>
+            </li>
+          </a>
+          <a href="">
+            <li class="nav-item flex">
+              <p class="ff-noto">行程</p>
+              <svg class="icon-arrow-forward svg">
+                <use
+                  xlink:href="./icomoon/symbol-defs.svg#icon-arrow_forward_ios"
+                ></use>
+              </svg>
+            </li>
+          </a>
+          <a href="">
+            <li class="nav-item flex">
+              <p class="ff-noto">商品</p>
+              <svg class="icon-arrow-forward svg">
+                <use
+                  xlink:href="./icomoon/symbol-defs.svg#icon-arrow_forward_ios"
+                ></use>
+              </svg>
+            </li>
+          </a>
+          <a href="">
+            <li class="nav-item flex">
+              <p class="ff-noto">成就紀錄</p>
+              <svg class="icon-arrow-forward svg">
+                <use
+                  xlink:href="./icomoon/symbol-defs.svg#icon-arrow_forward_ios"
+                ></use>
+              </svg>
+            </li>
+          </a>
+          <a href="">
+            <li class="nav-item flex">
+              <p class="ff-noto">登山記事</p>
+              <svg class="icon-arrow-forward svg">
+                <use
+                  xlink:href="./icomoon/symbol-defs.svg#icon-arrow_forward_ios"
+                ></use>
+              </svg>
+            </li>
+          </a>
+          <a href="">
+            <li class="nav-item flex">
+              <p class="ff-noto">難度分級</p>
+              <svg class="icon-arrow-forward svg">
+                <use
+                  xlink:href="./icomoon/symbol-defs.svg#icon-arrow_forward_ios"
+                ></use>
+              </svg>
+            </li>
+          </a>
+          <a href="">
+            <li class="nav-item flex">
+              <p class="ff-noto">天氣</p>
+              <svg class="icon-arrow-forward svg">
+                <use
+                  xlink:href="./icomoon/symbol-defs.svg#icon-arrow_forward_ios"
+                ></use>
+              </svg>
+            </li>
+          </a>
+          <a href="">
+            <li class="nav-item flex">
+              <p class="ff-noto">登山須知</p>
+              <svg class="icon-arrow-forward svg">
+                <use
+                  xlink:href="./icomoon/symbol-defs.svg#icon-arrow_forward_ios"
+                ></use>
+              </svg>
+            </li>
+          </a>
+          <!-- 可能使用javascript append生成 -->
+
+          <li class="nav-logo">
+            <svg class="icon-nomad-logo-dark navbar-logo-svg">
+              <use
+                xlink:href="./icomoon/symbol-defs.svg#icon-nomad-logo-dark"
+              ></use>
+            </svg>
+          </li>
+
+          <!-- 如果已登入、顯示此狀態-->
+          <!-- <a href="">
+              <button class="logout-btn ff-noto">登出</button>
+            </a> -->
+          <!-- P如果已登入、顯示此狀態 -->
+          <p class="text ff-noto">成為Nomad會員<br />開始你的專屬旅程</p>
+          <div class="btns flex">
+            <a href="">
+              <div class="signup-btn ff-noto">加入</div>
+            </a>
+            <a href="">
+              <div class="login-btn ff-noto">登入</div>
+            </a>
+          </div>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</div>
+
+<div class="modal"></div>
